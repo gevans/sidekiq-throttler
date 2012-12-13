d9ceeb658e26235885a600687d6636cc0edb011d
Gabriel Evans <gabriel@codeconcoction.com>
2012-12-13 07:01:07 -0800

    Add Sidekiq server middleware

--- lib/sidekiq/throttler.rb
+++ lib/sidekiq/throttler.rb
@@ -6,6 +6,36 @@ require 'sidekiq/throttler/version'
 require 'sidekiq/throttler/rate_limit'
 
 module Sidekiq
+  ##
+  # Sidekiq server middleware. Throttles jobs when they exceed limits specified
+  # on the worker. Jobs that exceed the limit are requeued with a delay.
   class Throttler
+
+    ##
+    # Passes the worker, arguments, and queue to {RateLimit} and either yields
+    # or requeues the job depending on whether the worker is throttled.
+    #
+    # @param [Sidekiq::Worker] worker
+    #   The worker the job belongs to.
+    #
+    # @param [Hash] msg
+    #   The job message.
+    #
+    # @param [String] queue
+    #   The current queue.
+    def call(worker, msg, queue)
+      rate_limit = RateLimit.new(worker, msg['args'], queue)
+
+      rate_limit.within_bounds do
+        yield
+      end
+
+      rate_limit.exceeded do |delay|
+        worker.class.perform_in(delay, *msg['args'])
+      end
+
+      rate_limit.execute
+    end
+
   end # Throttler
 end # Sidekiq
---
+++ spec/sidekiq/throttler_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe Sidekiq::Throttler do
+
+  subject(:throttler) do
+    described_class.new
+  end
+
+  let(:worker) do
+    LolzWorker.new
+  end
+
+  let(:message) do
+    {
+      args: 'Clint Eastwood'
+    }
+  end
+
+  let(:queue) do
+    'default'
+  end
+
+  describe '#call' do
+
+    it 'instantiates a rate limit with the worker, args, and queue' do
+      rate_limit = Sidekiq::Throttler::RateLimit.new(worker, message['args'], queue)
+      Sidekiq::Throttler::RateLimit.should_receive(:new).with(
+        worker, message['args'], queue
+      ).and_return(rate_limit)
+
+      throttler.call(worker, message, queue) {}
+    end
+
+    it 'yields in RateLimit#within_bounds' do
+      expect { |b| throttler.call(worker, message, queue, &b) }.to yield_with_no_args
+    end
+
+    it 'calls RateLimit#execute' do
+      Sidekiq::Throttler::RateLimit.any_instance.should_receive(:execute)
+      throttler.call(worker, message, queue)
+    end
+
+    context 'when rate limit is exceeded' do
+
+      it 'requeues the job with a delay' do
+        Sidekiq::Throttler::RateLimit.any_instance.should_receive(:exceeded?).and_return(true)
+        worker.class.should_receive(:perform_in).with(1.minute, *message['args'])
+        throttler.call(worker, message, queue)
+      end
+    end
+  end
+end
