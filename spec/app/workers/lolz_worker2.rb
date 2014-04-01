6e8c620f19390cea496efc159183032a019b86f7
Jeremy Blackburn <jeremy.blackburn@gmail.com>
2014-04-01 18:26:01 -0400

    removed some useless comments

--- spec/app/workers/lolz_worker2.rb
+++ spec/app/workers/lolz_worker2.rb
@@ -2,9 +2,7 @@ class LolzWorker2
   include Sidekiq::Worker
 
   # immediately requeue
-  sidekiq_options throttle: { threshold: 10, period: 1.minute, exceeded: Proc.new {|delay| worker.class.perform_async(payload) } }
-  # , when_exceeded:  ->(delay, worker, payload, queue) { puts "HURFFF"; puts "payload: #{payload}" } 
-  # { @worker.class.perform_async(@payload); puts "wtf2wtf2" }
+  sidekiq_options throttle: { threshold: 10, period: 1.minute, exceeded: Proc.new { |delay| worker.class.perform_async(payload) } }
 
   def perform(name)
     puts "LolzWorker2!!!"
